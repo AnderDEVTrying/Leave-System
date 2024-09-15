a0db14cc14e7d1d3e39d66ead66038e85e64cd3c
Anderson Zango <azango94@gmail.com>
2024-09-15 19:25:57 +0200

    Security Infra

---
+++ src/main/java/com/LeaveSystem/Repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.LeaveSystem.Repository;
+
+import com.LeaveSystem.Domain.User.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    UserDetails findByEmail(String email);
+
+}
