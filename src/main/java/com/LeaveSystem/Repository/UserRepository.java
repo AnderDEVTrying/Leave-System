455a637c8e49afd25358fe2df33504c8dd2db404
Anderson Zango <azango94@gmail.com>
2024-09-17 14:40:31 +0200

    Adding findByUsername

--- src/main/java/com/LeaveSystem/Repository/UserRepository.java
+++ src/main/java/com/LeaveSystem/Repository/UserRepository.java
@@ -4,10 +4,13 @@ import com.LeaveSystem.Domain.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     UserDetails findByEmail(String email);
+
+    Optional<User> findByUsername(String name);
 
 }
